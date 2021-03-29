c313a5dd2a541957e64b77488a96ddcf074e7eff
Polidoro-root <jv.polidoro@outlook.com>
2021-03-29 20:18:06 -0300

    Printer with webContents

---
+++ main.js
@@ -0,0 +1,34 @@
+const { app, BrowserWindow, webContents } = require('electron')
+const path = require('path')
+
+function createWindow() {
+	const window = new BrowserWindow({
+		width: 1024,
+		height: 768,
+		webPreferences: {
+			preload: path.join(__dirname, 'preload.js')
+		}
+	})
+
+	window.loadFile('index.html')
+	console.log(window.webContents.getPrinters())
+}
+
+app.whenReady().then(() => {
+	createWindow()
+
+
+
+	app.on('active', () => {
+		if (BrowserWindow.getAllWindows().length === 0) {
+			createWindow()
+			
+		}
+	})
+})
+
+app.on('window-all-closed', () => {
+	if (process.plataform !== 'darwin') {
+		app.quit()
+	}
+})
